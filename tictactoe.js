d29acfd8bc667df828970a11465113bc603586c0
Lyzacamille <lyzanorton40@gmail.com>
2018-07-02 10:32:01 -0400

    update

--- tictactoe.js
+++ tictactoe.js
@@ -5,14 +5,14 @@ var playerXSelections = new Array()
 var playerOSelections = new Array()
   
 const winningCombinations = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9],
-    [1,4,7],
-    [2,5,8],
-    [3,6,9],
-    [1,5,9],
-    [3,5,7]
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7]
 ]
 handleClick= function(event){
     var cell = event.target
@@ -20,6 +20,6 @@ handleClick= function(event){
 }
 var cells = document.querySelectorAll("td");
 
-for(var i = 0; i<cells.length;i++){
-    cells[i].addEventListener('click',handleClick)
+for(var i = 0; i < cells.length; i++){
+    cells[i].addEventListener('click', handleClick)
 }
